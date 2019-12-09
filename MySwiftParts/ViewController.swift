2ebe757621848400c25282983ed56134cfcc0832
letitride <ichikawa.fumiya@gmail.com>
2019-12-09 11:05:21 +0900

    Initial Commit

---
+++ MySwiftParts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySwiftParts
+//
+//  Created by 市川文也 on 2019/12/09.
+//  Copyright © 2019 Fumiya Ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
