5cf1a649578812c88b2b57d73009550be4057168
letitride <ichikawa.fumiya@gmail.com>
2019-12-10 08:48:54 +0900

    QRコード作成サンプル

--- MySwiftParts/ViewController.swift
+++ MySwiftParts/ViewController.swift
@@ -12,9 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let qrCImage = makeQRCode( str: "Sample" )
+        guard let qr = qrCImage else{
+            return
+        }
+        let imageView = UIImageView(image: UIImage(ciImage: qrCImage!))
+        imageView.frame.size.width = 200
+        imageView.frame.size.height = 200
+
+        imageView.center.x = self.view.frame.width / 2
+        imageView.center.y = self.view.frame.height / 2
+
+        self.view.addSubview(imageView)
     }
 
-
+    private func makeQRCode( str:String )->CIImage?{
+        let data = str.data(using: .utf8)
+        let qr = CIFilter(name: "CIQRCodeGenerator", parameters: ["inputMessage": data, "inputCorrectionLevel": "M"])
+        let sizeTransform = CGAffineTransform(scaleX: 10, y: 10)
+        if let qr = qr {
+            return qr.outputImage!.transformed(by: sizeTransform)
+        }
+        return nil
+    }
 }
 
