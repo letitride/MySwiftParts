565b822c600f04e28bbb71c57b76d44cba51a67d
letitride <ichikawa.fumiya@gmail.com>
2019-12-09 14:41:29 +0900

    textLabelの縦書き表示サンプル

--- MySwiftParts/ViewController.swift
+++ MySwiftParts/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import TTTAttributedLabel
 
 class ViewController: UIViewController {
 
+    var myLabel:TTTAttributedLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myLabel = TTTAttributedLabel(frame: CGRect(x: 0, y: 200, width: 500, height: 30))
+        myLabel.center.x = self.view.frame.width - 15 - 15
+        myLabel.center.y = 250 + 100
+
+        view.addSubview(myLabel)
+        
+        //myLabel.font = UIFont.systemFont(ofSize: 20)
+        //myLabel.textColor = UIColor.black
+        myLabel.backgroundColor = UIColor.red
+
+        //myLabel.numberOfLines = 0
+        myLabel.verticalAlignment = .top
+
+        let angle = CGFloat.pi/2
+        myLabel.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+
+        let text = "株式会社かぶ〜さん"
+
+        myLabel.setText(text) { (mutableAttributedString) -> NSMutableAttributedString? in
+            mutableAttributedString!.addAttribute(NSAttributedString.Key.init(rawValue: kCTVerticalFormsAttributeName as String), value: true, range: NSMakeRange(0, mutableAttributedString!.length))
+            return mutableAttributedString
+        }
+        
+        myLabelEditable()
     }
-
-
+    
+    let textField = UITextField()
+    private func myLabelEditable(){
+        myLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changeLabel)))
+        textField.isHidden = true
+        textField.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        view.addSubview(textField)
+    }
+    
+    @objc func changeLabel(){
+        textField.becomeFirstResponder()
+    }
+    
+    @objc func textFieldEditingChanged(_ textField: UITextField){
+        myLabel.setText(textField.text!) { (mutableAttributedString) -> NSMutableAttributedString? in
+            mutableAttributedString!.addAttribute(NSAttributedString.Key.init(rawValue: kCTVerticalFormsAttributeName as String), value: true, range: NSMakeRange(0, mutableAttributedString!.length))
+            return mutableAttributedString
+        }
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MySwiftParts' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'TTTAttributedLabel'
+
+  # Pods for MySwiftParts
+
+  target 'MySwiftPartsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MySwiftPartsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
