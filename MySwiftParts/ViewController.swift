69fe53a7eecea6e85eccddc99ad1dfb244338caa
letitride <ichikawa.fumiya@gmail.com>
2020-02-12 11:07:35 +0900

    view中心間の線まで描画

--- MySwiftParts/ViewController.swift
+++ MySwiftParts/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let view1 = MView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
-    let view2 = MView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    let view2 = MView(frame: CGRect(x: 200, y: 200, width: 100, height: 100))
 
     
     class MView: UIView {
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
         var parent:UIView = UIView()
         override init(frame: CGRect) {
             super.init(frame: frame)
+            self.layer.borderColor = UIColor.red.cgColor
+            self.layer.borderWidth = 5
+            self.layer.backgroundColor = UIColor.white.cgColor
+            self.layer.cornerRadius = 50
         }
 
         required init?(coder aDecoder: NSCoder) {
@@ -38,13 +42,26 @@ class ViewController: UIViewController {
 
     }
     
+    let line1 = UIBezierPath()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let shapeLayer = CAShapeLayer()
+        line1.move(to: view1.center)
+        line1.addLine(to: view2.center)
+        line1.close()
+        line1.stroke()
+        shapeLayer.strokeColor = UIColor.blue.cgColor
+        shapeLayer.path = line1.cgPath
+        view.layer.addSublayer(shapeLayer)
         
-        view1.layer.borderColor = UIColor.red.cgColor
-        view1.layer.borderWidth = 10
-        view2.layer.borderColor = UIColor.red.cgColor
-        view2.layer.borderWidth = 10
+        line1.move(to: CGPoint(x: 200, y: 300))
+        line1.addLine(to: CGPoint(x: 300, y: 400))
+        line1.stroke()
+        shapeLayer.path = line1.cgPath
+
         view1.addView(parent: self.view)
         view2.addView(parent: self.view)
     }
