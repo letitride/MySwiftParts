cce8d810a035caa39fbfc8ce769815629f13baf9
letitride <ichikawa.fumiya@gmail.com>
2020-02-12 11:42:46 +0900

    view移動処理をdelegateに移譲

--- MySwiftParts/ViewController.swift
+++ MySwiftParts/ViewController.swift
@@ -8,53 +8,48 @@
 
 import UIKit
 
+protocol MViewDelegate{
+    func move(_ touches: Set<UITouch>, with event: UIEvent?)
+}
+
+class MView: UIView {
+    
+    var delegate:MViewDelegate?
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.borderColor = UIColor.red.cgColor
+        self.layer.borderWidth = 5
+        self.layer.backgroundColor = UIColor.white.cgColor
+        self.layer.cornerRadius = 50
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.delegate?.move( touches, with: event)
+    }
+}
+
 class ViewController: UIViewController {
 
     let view1 = MView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
     let view2 = MView(frame: CGRect(x: 200, y: 200, width: 100, height: 100))
-
-    
-    class MView: UIView {
-        
-        var parent:UIView = UIView()
-        override init(frame: CGRect) {
-            super.init(frame: frame)
-            self.layer.borderColor = UIColor.red.cgColor
-            self.layer.borderWidth = 5
-            self.layer.backgroundColor = UIColor.white.cgColor
-            self.layer.cornerRadius = 50
-        }
-
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-        }
-
-        func addView(parent:UIView){
-            self.parent = parent
-            parent.addSubview(self)
-        }
-        
-        override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-            guard let touch = touches.first, let view = touch.view else { return }
-            let location = touch.location(in: parent)
-            view.center = location
-        }
-
-    }
     
     let line1 = UIBezierPath()
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        view1.delegate = self
+        view2.delegate = self
 
         let shapeLayer = CAShapeLayer()
         line1.move(to: view1.center)
         line1.addLine(to: view2.center)
-        line1.close()
         line1.stroke()
         shapeLayer.strokeColor = UIColor.blue.cgColor
-        shapeLayer.path = line1.cgPath
+        //shapeLayer.path = line1.cgPath
         view.layer.addSublayer(shapeLayer)
         
         line1.move(to: CGPoint(x: 200, y: 300))
@@ -62,9 +57,20 @@ class ViewController: UIViewController {
         line1.stroke()
         shapeLayer.path = line1.cgPath
 
-        view1.addView(parent: self.view)
-        view2.addView(parent: self.view)
+        self.view.addSubview(view1)
+        self.view.addSubview(view2)
+
     }
     
+}
+
+extension ViewController:MViewDelegate{
+    
+    func move(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first, let view = touch.view else { return }
+        let location = touch.location(in: self.view)
+        view.center = location
+    }
+
 }
 
